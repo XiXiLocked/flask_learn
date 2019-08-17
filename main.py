4b7ee6d6354d0f0616e6cc569b9afb3ab27ec39b
XiXiLocked <xijunyi1990@gmail.com>
2019-08-17 22:06:48 +0800

    chap4

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from flask import Flask
+from flask import session, redirect, url_for, flash
+
 from flask import render_template
 from flask_bootstrap import Bootstrap
+
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY']='0101010101010101'
 booststrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField("What's your name?",validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/',methods = ['GET','POST'])
 def index():
-    print(datetime.utcnow())
-    return render_template('index.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        oname = session.get('name')
+        if oname is not None and oname != form.name.data:
+            flash('Looks like you have changed your name')
+        session['name']= form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time = datetime.utcnow(), form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
