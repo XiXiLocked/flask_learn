8e9da5f04890924222e4a3b88427d880918ae6d9
XiXiLocked <xijunyi1990@gmail.com>
2019-08-18 16:47:33 +0800

    chap 5 database

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import session, redirect, url_for, flash
 
@@ -11,10 +12,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY']='0101010101010101'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 booststrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class NameForm(FlaskForm):
     name = StringField("What's your name?",validators=[DataRequired()])
@@ -24,12 +34,22 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        oname = session.get('name')
-        if oname is not None and oname != form.name.data:
-            flash('Looks like you have changed your name')
-        session['name']= form.name.data
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known' ] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data= ''
         return redirect(url_for('index'))
-    return render_template('index.html', current_time = datetime.utcnow(), form = form, name = session.get('name'))
+    return render_template('index.html',
+            current_time = datetime.utcnow(),
+            form = form,
+            name = session.get('name'),
+            known = session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -46,3 +66,25 @@ def internal_server_error(e):
 @app.route('/der')
 def der():
     return render_template('der.html')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r' %self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    roleid = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return 'User %r' % self.username
+
+#@app.shell_context_processor
+def make_shell_context():
+    return dict(db = db , User=User, Role=Role)
